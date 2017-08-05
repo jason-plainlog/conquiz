a8a79d154ea2834e3fded370a8170cd62b088bca
Jason <jason.plainlog@gmail.com>
2017-08-05 21:39:38 +0800

    Add Signup function

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,21 @@
 var express = require('express');
+var User = require('../models/user.js');
 var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  console.log(req.body['email']);
-  console.log(req.body['password']);
-  console.log(req.body['value']);
-  res.send('success');
+  if(req.body['submit'] == 'signup'){
+    var newUser = new User({
+      email: req.body['email'],
+      password: req.body['password']
+    });
+    newUser.save();
+  }else{
+    console.log(req.body['email']);
+    console.log(req.body['password']);
+    console.log(req.body['submit']);
+  }
+  res.render('index', { title: 'Conquiz' });
 });
 
 router.get('/', function(req, res, next) {
