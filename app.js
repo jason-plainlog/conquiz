ea3cb182fd10d6a6ff3ddc0212c2903664528fcc
Jason <jason.plainlog@gmail.com>
2017-08-05 17:16:36 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var express = require('express');
+
+var index = require('./routes/index');
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+app.use(function(req, res, next) {
+  res.status(404).send('404 Not Found.');
+});
+
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
+
+module.exports = app;
