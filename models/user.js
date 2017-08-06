f40c7a4e07df2db7994c3cf1b0402a55026645bf
Jason <jason.plainlog@gmail.com>
2017-08-06 12:14:07 +0800

    Add Signup function

--- models/user.js
+++ models/user.js
@@ -2,16 +2,25 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('models/user.db');
 
 db.serialize(function() {
-  db.run("CREATE TABLE IF NOT EXISTS user(email VARCHAR(32), password VARCHAR(32))");
+  db.run("CREATE TABLE IF NOT EXISTS user(username VARCHAR UNIQUE, password VARCHAR)");
 });
 
 function User(user){
-  this.email = user.email;
+  this.username = user.username;
   this.password = user.password;
 };
 
+User.prototype.exist = function exist(user, callback){
+  db.get("SELECT COUNT(rowid) FROM user WHERE username=?", this.username, function(err, row) {
+    if(err)
+      console.log(err);
+    if(callback)
+      callback(user, row['COUNT(rowid)']);
+  });
+}
+
 User.prototype.save = function save(callback){
-  db.run("INSERT INTO user(email, password) VALUES(?, ?)", this.email, this.password);
+  db.run("INSERT INTO user(username, password) VALUES(?, ?)", this.username, this.password);
 };
 
 module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,35 @@
 var express = require('express');
+var crypto = require('crypto');
 var User = require('../models/user.js');
 var router = express.Router();
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
-  if(req.body['submit'] == 'signup'){
+  if(req.body['username'] == '' || req.body['password'] == '')
+    res.render('index', { title: 'Conquiz', error: "Username / Password can't be blank!", username: req.body['username']});
+  else if(req.body['submit'] == 'signup'){
     var newUser = new User({
-      email: req.body['email'],
-      password: req.body['password']
+      username: req.body['username'].toLowerCase(),
+      password: crypto.createHash('md5').update(req.body['password']).digest('hex')
     });
-    newUser.save();
+    newUser.exist(newUser, function(user, result){
+      if(result == 1)
+        res.render('index', { title: 'Conquiz', error: newUser.username + ' had been used!', username: newUser.username});
+      else{
+        user.save();
+        res.render('index', { title: 'Conquiz', error: 'Success!' });
+      }
+    });
   }else{
-    console.log(req.body['email']);
+    console.log(req.body['username']);
     console.log(req.body['password']);
     console.log(req.body['submit']);
+    res.render('index', { title: 'Conquiz'});
   }
-  res.render('index', { title: 'Conquiz' });
 });
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Conquiz' });
+  res.render('index', { title: 'Conquiz'});
 });
 
 module.exports = router;
